b740d3f292b93f92293d70239b51444ffb6d568f
Chen <qiongchen@china.telenav.com>
2018-06-28 10:34:32 +0800

    添加了搜索框的一个浮动离左边框5px

--- src/components/rodesearch/search.vue
+++ src/components/rodesearch/search.vue
@@ -2,7 +2,7 @@
 div
   gmap-map(   ref="googleMap"  :center="center" :zoom="zoom" )
   div.search-box
-    Input.ml10(type="text" icon="search" v-model="key" style="width:200px;height:0px" placeholder="编号" @on-enter="search()" @on-click="search()")
+    Input.ml10(type="text" icon="search" v-model="key" style="width:200px;height:0px;left:5px" placeholder="编号" @on-enter="search()" @on-click="search()")
   vue-googlemap-polyline( ref="ployline" v-for="(m, index) in ploys" class="google-ployline" :map="map" :key="index" :path="m" :strokeColor="color"
   :strokeWeight="15" :events="event" @ploylineClick="addLatLng" @onclick="addLatLng" :editable="false")
   vue-google-info-window(v-for="(m, key) in marks" :key="key+'info'" :content="m.content" :map="map" :position="m.position"  :opened="true" )
